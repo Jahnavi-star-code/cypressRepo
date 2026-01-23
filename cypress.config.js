9b1494361c1c59b0e6f3e5425b04446e18b9c04a
Jahnavi-star-code <reddyjanu45@gmail.com>
2026-01-23 09:39:20 +0530

    re modifying config file

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "https://petstore.swagger.io/v2",
+    numTestsKeptInMemory: 0,
+    watchForFileChanges: true,
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import userfun from "../pageObjects/apiTesting/userfun";
+describe("template spec", () => {
+  it("passes", () => {
+    userfun.addPet();
+  });
+  it("getUser", () => {
+    userfun.getUser();
+  });
+});
